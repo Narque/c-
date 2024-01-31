790cf312c8a5b68c8a042a3239d3e2f2f60d167d
Narque <37028415+Narque@users.noreply.github.com>
2024-01-31 23:59:09 +0300

    Add files via upload

---
+++ ORT_HESAPLAMA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Not_Bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad, bolum, program;
+            int s1, s2, s3, ort;
+
+            ad ="Emre";
+            bolum ="Mekatronik";
+            program ="Sayisal Elektronik";
+
+            s1 = 23;
+            s2 = 31;
+            s3 = 52;
+            ort = (s1 + s2 + s3) / 3 ;
+            Console.WriteLine("Adi:"+ ad );
+            Console.WriteLine("Bolumu:" + bolum);
+            Console.WriteLine("Ders" + program);
+            Console.WriteLine();
+            Console.WriteLine("Birinci Sinav:" + s1 );
+            Console.WriteLine("İkinci Sinav:" + s2 );
+            Console.WriteLine("Ucuncu Sinav:" +  s3);
+            Console.WriteLine("Ortalama:" + ort);
+            Console.ReadLine();
+
+        }
+    }
+}
